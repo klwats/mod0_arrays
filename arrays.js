33ad5836c9a716c2368c29e0b4d9a998ba3c6090
Kelli Watkins <watkinskellil@gmail.com>
2022-10-19 14:27:41 -0600

    Add array calls

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var firstNames = ["anne", "kelli", "lucy", "mabel"]
 var areaCodes = [405, 055, 705, 263]
 var trues = [true, true, true, true]
+Removing a name from the variable
+firstNames.pop()
+console.log(firstNames);
+Adding to the end of the Array
+areaCodes.push(999);
+console.log(areaCodes)
+Add to the beginning of Array
+trues.unshift(false)
+console.log(trues)
