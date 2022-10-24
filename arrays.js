e855647172330d82bbe479974086bd25de0b0db9
Kelli Watkins <watkinskellil@gmail.com>
2022-10-23 19:37:40 -0600

    Comment out statements

--- arrays.js
+++ arrays.js
@@ -1,14 +1,14 @@
 var firstNames = ["anne", "kelli", "lucy", "mabel"]
 var areaCodes = [405, 055, 705, 263]
 var trues = [true, true, true, true]
-Removing a name from the variable
+//Removing a name from the variable
 firstNames.pop()
 console.log(firstNames);
-Adding to the end of the Array
+//Adding to the end of the Array
 areaCodes.push(999);
 console.log(areaCodes)
-Add to the beginning of Array
+//Add to the beginning of Array
 trues.unshift(false)
 console.log(trues)
 
-Index positions is the position in the Array, starting with 0; length of an array will always be index+1
+//Index positions is the position in the Array, starting with 0; length of an array will always be index+1
